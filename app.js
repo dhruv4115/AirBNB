cd38d994e01f578a86b88967c5a19626236917f0
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-10-21 01:18:22 +0530

    Delete Listing Route added

--- app.js
+++ app.js
@@ -86,6 +86,14 @@ app.put("/listings/:id", async (req, res) => {
     }
 });
 
+//Delete Route
+app.delete("/listings/:id" , async (req,res) =>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
 /*
 app.get("/testListing", async (req, res) => {
     try {
